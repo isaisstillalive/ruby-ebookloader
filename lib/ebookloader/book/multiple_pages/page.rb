252f140f720d427398c0f272d6125dceb896eb6f
isaisstillalive <isaisstillalive@gmail.com>
2014-02-23 01:35:47 +0900

    MultiplePages#pagesの戻り値で使用するためのPageクラスを作成

---
+++ lib/ebookloader/book/multiple_pages/page.rb
@@ -0,0 +1,50 @@
+# coding: utf-8
+
+module EBookloader
+    class Book
+    	module MultiplePages
+	    	class Page
+	    		attr_reader :uri
+
+	    		def initialize uri, options = {}
+	    			@uri = URI(uri)
+	    			@options = options
+	    		end
+
+	    		def name
+	    			@options[:name]
+	    		end
+
+	    		def extension
+	    			if @options[:extension]
+	    				@options[:extension]
+	    			else
+	    				extension = Pathname(uri.path).extname
+	    				if extension.empty?
+	    					:jpg
+	    				else
+	    					extension[1..-1].to_sym
+	    				end
+	    			end
+	    		end
+
+	    		def filename page
+	    			format = if name
+	    				'%1$03d_%3$s.%2$s'
+	    			else
+	    				'%1$03d.%2$s'
+	    			end
+	    			format % [page, extension, name]
+	    		end
+
+            def == other
+                return false unless self.uri == other.uri
+                return false unless self.name == other.name
+                return false unless self.extension == other.extension
+
+                true
+            end
+	    	end
+    	end
+    end
+end
