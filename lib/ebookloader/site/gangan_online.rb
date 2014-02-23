41e1590050558aeae919c0f082e2749e66ff35e7
isaisstillalive <isaisstillalive@gmail.com>
2014-02-24 00:15:13 +0900

    ガンガンオンラインの抽出ロジックを修正

--- lib/ebookloader/site/gangan_online.rb
+++ lib/ebookloader/site/gangan_online.rb
@@ -14,10 +14,12 @@ module EBookloader
                     @name = source.body.match(%r{<h2 class="iepngFixBg">(?<title>.*?) <span class="titleYomi">\((.*?)\)</span></h2>})[:title]
                 end
 
-                @books = lazy_collection source.body, %r{<li(?: class="last")?>【(?<episode_num>[^】]*?)】(?<episode>.*?)：<a href="javascript:void\(0\);" onclick="javascript:Fullscreen\('(?<uri>[^']*?)'\);">PC</a>.*?</li>}m do |sc|
-                    uri = @uri + sc[:uri]
-                    name = '%s %s %s' % [@name, sc[:episode_num], sc[:episode]]
-                    Book::ActiBook.new(uri, name: name)
+                source.body.match %r{<ul id="contentCenterBknmbrList">.*?</ul>}m do |match|
+                    @books = lazy_collection match[0], %r{<li(?: class="last")?>【(?<episode_num>[^】]*?)】(?<episode>.*?)：<a [^>]*?onclick="javascript:Fullscreen\('(?<uri>[^']*?)'\);"[^>]*?>PC</a>.*?</li>}m do |sc|
+                        uri = @uri + sc[:uri]
+                        name = '%s %s %s' % [@name, sc[:episode_num], sc[:episode]]
+                        Book::ActiBook.new(uri, name: name)
+                    end
                 end
 
                 true
