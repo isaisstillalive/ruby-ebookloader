0fadc83b6e2115d0c494af41f5bc7bd6bfa5c414
isaisstillalive <isaisstillalive@gmail.com>
2014-02-23 19:01:39 +0900

    Connectable#getメソッドにヘッダを渡せるように修正

--- lib/ebookloader/connectable.rb
+++ lib/ebookloader/connectable.rb
@@ -4,15 +4,14 @@ module EBookloader
     module Connectable
         protected
 
-        def get uri, params = nil
+        def get uri, options = {}
             conn = Faraday.new url: (uri.scheme + '://' + uri.host) do |faraday|
                 faraday.request :url_encoded
                 # faraday.response :logger
                 faraday.adapter Faraday.default_adapter
             end
-            conn.get uri.request_uri, params do |g|
-                # g.headers['User-Agent'] = 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/32.0.1700.107 Safari/537.36'
-                # g.headers['Referer'] = uri.to_s
+            conn.get uri.request_uri, options[:params] do |g|
+                g.headers.merge! options[:headers] if options[:headers]
                 g.headers['Connection'] = 'Keep-Alive';
             end
         end
--- spec/conneclable_spec.rb
+++ spec/conneclable_spec.rb
@@ -10,20 +10,45 @@ describe EBookloader::Connectable do
     let(:connectable_object){ ConnectableObject.new }
 
     describe '#get' do
-    	subject{ connectable_object.__send__ :get, URI('http://example.com/path'), {param: :param} }
+    	subject{ connectable_object.__send__ :get, URI('http://example.com/path') }
         let(:faraday) { double 'faraday' }
         let(:conn){ double 'conn' }
         let(:get_options){ double 'get_options', headers: headers }
         let(:headers){ double 'headers' }
+        before{
+            allow( Faraday ).to receive(:new).with(url: 'http://example.com').and_return(conn)
+            allow( headers ).to receive(:[]=).with('Connection', 'Keep-Alive')
+        }
 
         it 'はFaraday#getを実行する' do
             expect( Faraday ).to receive(:new).with(url: 'http://example.com').and_yield(faraday).and_return(conn)
             expect( faraday ).to receive(:request).with(:url_encoded)
             expect( faraday ).to receive(:adapter).with(Faraday.default_adapter)
-            expect( conn ).to receive(:get).with('/path', {param: :param}).and_yield(get_options).and_return( double('responce', {:body => 'body'}) )
+            expect( conn ).to receive(:get).with('/path', nil).and_yield(get_options).and_return( double('responce', {:body => 'body'}) )
             expect( headers ).to receive(:[]=).with('Connection', 'Keep-Alive')
 
-            subject
+            expect( subject.body ).to eql 'body'
+        end
+
+        context 'パラメータが渡されている場合' do
+            subject{ connectable_object.__send__ :get, URI('http://example.com/path'), params: {param: :param} }
+
+            it 'は#getにパラメータを渡す' do
+                expect( conn ).to receive(:get).with('/path', param: :param)
+                
+                subject
+            end
+        end
+
+        context 'ヘッダが渡されている場合' do
+            subject{ connectable_object.__send__ :get, URI('http://example.com/path'), headers: {header: :header} }
+
+            it 'は#getのオプションにヘッダを結合する' do
+                allow( conn ).to receive(:get).with('/path', nil).and_yield(get_options).and_return( double('responce', {:body => 'body'}) )
+                expect( headers ).to receive(:merge!).with({header: :header})
+                
+                subject
+            end
         end
     end
 end
