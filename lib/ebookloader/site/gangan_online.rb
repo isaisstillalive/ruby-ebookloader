120a02af056b5806463975e3d8cb0a1aad13f141
isaisstillalive <isaisstillalive@gmail.com>
2014-02-24 03:39:36 +0900

    ガンガンオンラインのサイトにて本以外の行を読み込まないように修正
    本の行を取り漏らしていたので修正

--- lib/ebookloader/site/gangan_online.rb
+++ lib/ebookloader/site/gangan_online.rb
@@ -13,7 +13,7 @@ module EBookloader
                 self.merge! source.body.match(%r{<h2 class="iepngFixBg">(?<title>.*?)\s*<span class="titleYomi">\s*\((.*?)\)</span></h2>})
 
                 source.body.match %r{<ul id="contentCenterBknmbrList">.*?</ul>}m do |match|
-                    @books = lazy_collection source.body, %r{<li(?: class="last")?>【(?<episode_num>[^】]*?)】(?<episode>.*?)：<a [^>]*?onclick="javascript:Fullscreen\('(?<uri>[^']*?)'\);"[^>]*?>PC</a>.*?</li>}m do |sc|
+                    @books = lazy_collection source.body, %r{<li[^>]*?>【(?<episode_num>[^】]*?)】(?<episode>.*?)：<a [^>]*?onclick="javascript:Fullscreen\('(?<uri>[^']*?)'\);"[^>]*?>PC</a>} do |sc|
                         uri = @uri + sc[:uri]
                         name = '%s %s %s' % [self.name, sc[:episode_num], sc[:episode]]
                         Book::ActiBook.new(uri, name: name)
