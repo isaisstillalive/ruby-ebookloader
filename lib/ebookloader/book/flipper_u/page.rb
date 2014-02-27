c4972767320a0a6ccd752ca612bbbb63e39c113d
isaisstillalive <isaisstillalive@gmail.com>
2014-02-27 20:36:27 +0900

    FLIPPER Uで一時ファイルを作成しないように修正
    スペックがまたひどくなった

--- lib/ebookloader/book/flipper_u/page.rb
+++ lib/ebookloader/book/flipper_u/page.rb
@@ -11,32 +11,20 @@ module EBookloader
           baseUri = @uri + "./x#{@options[:scale]}/"
 
           index = 1
-          v_files = sliced_files(@options[:height], dir, "#{page}_0_%d.tmp")
-          v_files.each.with_index 1 do |v_file, y|
-            h_files = sliced_files(@options[:width], dir, "#{page}_%d_#{y}.tmp")
-            h_files.each.with_index 1 do |h_file, x|
+          imagelist = Magick::ImageList.new
+          @options[:height].times do
+            h_imagelist = Magick::ImageList.new
+            @options[:width].times do
               uri = baseUri + "./#{index}.#{@options[:extension]}"
-              write h_file, uri, @options
+              h_imagelist.from_blob get(uri, @options).body
               index += 1
             end
-
-            join v_file, h_files, false
+            imagelist << h_imagelist.append(false)
           end
 
           file = dir + filename(page)
-          join file, v_files, true
-        end
-
-        private
-        def join output_path, join_paths, is_vertically
-            imagelist = Magick::ImageList.new(*join_paths).append is_vertically
-            imagelist.write output_path
-
-            join_paths.each{ |file| file.delete }
-        end
-
-        def sliced_files count, dir, filename_format
-          (1..count).map{ |i| dir + filename_format % i }
+          image = imagelist.append(true)
+          image.write file
         end
       end
     end
--- spec/book/flipper_u/page_spec.rb
+++ spec/book/flipper_u/page_spec.rb
@@ -13,66 +13,32 @@ describe EBookloader::Book::FlipperU::Page do
     subject{ page.save 1, save_dir }
 
     it 'は枚数分#writeを実行して結合する' do
-      expect( page ).to receive(:sliced_files).with(4, save_dir, '1_0_%d.tmp').and_return([Pathname('dirname/1_0_1.tmp'), Pathname('dirname/1_0_2.tmp')])
-      expect( page ).to receive(:sliced_files).with(3, save_dir, '1_%d_1.tmp').and_return([Pathname('dirname/1_1_1.tmp'), Pathname('dirname/1_2_1.tmp')])
-      expect( page ).to receive(:sliced_files).with(3, save_dir, '1_%d_2.tmp').and_return([Pathname('dirname/1_1_2.tmp'), Pathname('dirname/1_2_2.tmp')])
-      expect( page ).to receive(:write).with(Pathname("dirname/1_1_1.tmp"), URI("http://example.com/dir/page1/x2/1.jpg"), options)
-      expect( page ).to receive(:write).with(Pathname("dirname/1_2_1.tmp"), URI("http://example.com/dir/page1/x2/2.jpg"), options)
-      expect( page ).to receive(:write).with(Pathname("dirname/1_1_2.tmp"), URI("http://example.com/dir/page1/x2/3.jpg"), options)
-      expect( page ).to receive(:write).with(Pathname("dirname/1_2_2.tmp"), URI("http://example.com/dir/page1/x2/4.jpg"), options)
-      expect( page ).to receive(:join).with(Pathname('dirname/1_0_1.tmp'), [Pathname('dirname/1_1_1.tmp'), Pathname('dirname/1_2_1.tmp')], false)
-      expect( page ).to receive(:join).with(Pathname('dirname/1_0_2.tmp'), [Pathname('dirname/1_1_2.tmp'), Pathname('dirname/1_2_2.tmp')], false)
-      expect( page ).to receive(:join).with(Pathname('dirname/1.jpg'), [Pathname('dirname/1_0_1.tmp'), Pathname('dirname/1_0_2.tmp')], true)
+      h_imagelist = double('Magick::ImageList')
+      v_imagelist = double('Magick::ImageList')
+      expect( Magick::ImageList ).to receive(:new).and_return(v_imagelist, h_imagelist, h_imagelist, h_imagelist, h_imagelist)
+
+      v_image = double('Magick::ImageList')
+      h_image_1 = double('Magick::ImageList')
+      h_image_2 = double('Magick::ImageList')
+      h_image_3 = double('Magick::ImageList')
+      h_image_4 = double('Magick::ImageList')
+      expect( h_imagelist ).to receive(:append).with(false).and_return(h_image_1, h_image_2, h_image_3, h_image_4)
+      expect( v_imagelist ).to receive(:append).with(true).and_return(v_image)
+
+      expect( v_imagelist ).to receive(:<<).with(h_image_1)
+      expect( v_imagelist ).to receive(:<<).with(h_image_2)
+      expect( v_imagelist ).to receive(:<<).with(h_image_3)
+      expect( v_imagelist ).to receive(:<<).with(h_image_4)
+
+      expect( v_image ).to receive(:write).with(Pathname('dirname/1.jpg'))
+
+      1.upto 12 do |i|
+        expect( page ).to receive(:get).with(URI("http://example.com/dir/page1/x2/#{i}.jpg"), options).and_return(double('Responce', body: "#{i}"))
+        expect( h_imagelist ).to receive(:from_blob).with("#{i}")
+      end
       expect( page ).to receive(:filename).with(1).and_return('1.jpg')
 
       subject
-    end
-  end
-
-  describe '#join' do
-    subject{ page.__send__ :join, Pathname('output'), [path1, path2, path3], true }
-
-    let(:path1){ Pathname('1') }
-    let(:path2){ Pathname('2') }
-    let(:path3){ Pathname('3') }
-    let(:imagelist){ double('Magick::ImageList') }
-    let(:imagelist2){ double('Magick::ImageList') }
-    before{
-      allow( Magick::ImageList ).to receive(:new).with(path1, path2, path3).and_return(imagelist)
-      allow( imagelist ).to receive(:append).with(true).and_return( imagelist2 )
-      allow( imagelist2 ).to receive(:write).with(Pathname('output'))
-
-      allow( path1 ).to receive(:delete)
-      allow( path2 ).to receive(:delete)
-      allow( path3 ).to receive(:delete)
-    }
-
-    it 'は結合する' do
-      expect( Magick::ImageList ).to receive(:new).with(path1, path2, path3).and_return(imagelist)
-      expect( imagelist ).to receive(:append).with(true).and_return( imagelist2 )
-      expect( imagelist2 ).to receive(:write).with(Pathname('output'))
-
-      subject
-    end
-
-    it 'は元ファイルを削除する' do
-      expect( path1 ).to receive(:delete).once
-      expect( path2 ).to receive(:delete).once
-      expect( path3 ).to receive(:delete).once
-
-      subject
-    end
-  end
-
-  describe '#sliced_files' do
-    subject{ page.__send__ :sliced_files, 3, Pathname('dirname'), "file_%d.tmp" }
-
-    it 'はファイル名テンプレートをcount数繰り返した配列を返す' do
-      expect( subject ).to eql [
-        Pathname('dirname/file_1.tmp'),
-        Pathname('dirname/file_2.tmp'),
-        Pathname('dirname/file_3.tmp'),
-      ]
     end
   end
 end
