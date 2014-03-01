47f9b8918ef676747c9c78621f5ff1359b006aa1
isaisstillalive <isaisstillalive@gmail.com>
2014-03-02 02:22:16 +0900

    スペックを修正

--- spec/book/multiple_pages_spec.rb
+++ spec/book/multiple_pages_spec.rb
@@ -28,59 +28,55 @@ describe EBookloader::Book::MultiplePages do
     end
   end
 
-  describe '#save_core(dirname)' do
+  describe '#save_core' do
     let(:dir_path){ Pathname('dirname') }
     let(:save_dir_path){ Pathname('dirname/name') }
     subject{ book.__send__ :save_core, dir_path }
 
-    let(:page1){ EBookloader::Book::MultiplePages::Page.new '1' }
-    let(:page2){ EBookloader::Book::MultiplePages::Page.new '2' }
+    let(:options){ {} }
+
+    let(:page){ double('Page') }
 
     before{
       allow( book ).to receive(:name).and_return('name')
-      allow( book ).to receive(:pages).and_return([])
-      allow( book ).to receive(:options).and_return({})
+      allow( book ).to receive(:options).and_return(options)
+      allow( book ).to receive(:pages).and_return([page])
 
-      allow( dir_path ).to receive(:+).and_return(save_dir_path)
       allow( save_dir_path ).to receive(:mkdir)
+      allow( save_dir_path ).to receive(:exist?).and_return(true)
 
-      allow( page1 ).to receive(:save)
-      allow( page2 ).to receive(:save)
+      allow( dir_path ).to receive(:+).with('name').and_return(save_dir_path)
+
+      allow( page ).to receive(:save)
+      allow( page ).to receive(:is_a?).with(EBookloader::Book::MultiplePages::Page).and_return(true)
+      allow_any_instance_of( EBookloader::Book::MultiplePages::Page ).to receive(:save)
     }
 
     it 'はtrueを返す' do
       expect( subject ).to eql true
     end
 
-    it 'はdirname/nameを保存ディレクトリとして使用する' do
-      expect( book ).to receive(:name).and_return('name')
-      expect( dir_path ).to receive(:+).with('name').and_return(save_dir_path)
+    it 'は保存先パスに本の名前を足して保存ディレクトリとして使用する' do
+      expect( page ).to receive(:save).with(1, save_dir_path)
       subject
     end
 
     it 'はpagesの数だけPage#saveを実行する' do
+      page1 = double('Page')
+      page2 = double('Page')
       expect( book ).to receive(:pages).and_return([page1, page2])
+      expect( page1 ).to receive(:is_a?).with(EBookloader::Book::MultiplePages::Page).and_return(true)
+      expect( page2 ).to receive(:is_a?).with(EBookloader::Book::MultiplePages::Page).and_return(true)
       expect( page1 ).to receive(:save).with(1, save_dir_path)
       expect( page2 ).to receive(:save).with(2, save_dir_path)
       subject
     end
 
-    context 'pagesの戻り値がPageではない場合' do
-      it 'はPageを作成してから使用する' do
-        allow( book ).to receive(:pages).and_return(['1', '2'])
-        expect( EBookloader::Book::MultiplePages::Page ).to receive(:new).with('1').and_return(page1).ordered
-        expect( EBookloader::Book::MultiplePages::Page ).to receive(:new).with('2').and_return(page2).ordered
+    context 'オプションとしてオフセットが指定されている場合' do
+      let(:options){ {offset: 2} }
 
-        subject
-      end
-    end
-
-    context 'オプションにオフセットが指定されている場合' do
       it 'はそのページ番号から保存を開始する' do
-        expect( book ).to receive(:options).and_return({offset: 2})
-        expect( book ).to receive(:pages).and_return([page1, page2])
-        expect( page1 ).to receive(:save).with(2, save_dir_path).ordered
-        expect( page2 ).to receive(:save).with(3, save_dir_path).ordered
+        expect( page ).to receive(:save).with(2, save_dir_path)
         subject
       end
     end
