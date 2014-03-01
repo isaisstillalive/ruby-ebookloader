f5ff5111598f6465b6f91cf2a2490dd187c4715e
isaisstillalive <isaisstillalive@gmail.com>
2014-03-02 06:03:40 +0900

    文字列クラスにextendしてメソッドを拡張するモジュールを追加
    グローバルマッチを行いMatchDataを返すscanメソッドの置き場所に困っていたがここに決定

---
+++ lib/ebookloader/string_extender.rb
@@ -0,0 +1,21 @@
+# coding: utf-8
+
+module EBookloader
+  module StringExtender
+    def global_match pattern
+      return to_enum(:global_match, pattern) unless block_given?
+
+      pos = 0
+      loop do
+        match = pattern.match(self, pos)
+        break if match.nil?
+
+        pos = match.offset(0)[1]
+
+        yield match
+      end
+
+      self
+    end
+  end
+end
---
+++ spec/string_extender_spec.rb
@@ -0,0 +1,49 @@
+# coding: utf-8
+
+require_relative 'spec_helper.rb'
+
+describe EBookloader::StringExtender do
+  let(:string){ 'abcdef' }
+  let(:extended_string){ string.extend EBookloader::StringExtender }
+
+  describe '#global_match' do
+    let(:regexp){ /(?<char>...)/ }
+
+    let(:match1){ string.match /(?<char>...)/, 0 }
+    let(:match2){ string.match /(?<char>...)/, 3 }
+    let(:match3){ string.match /(?<char>...)/, 6 }
+
+    context 'ブロックを渡さなかった場合' do
+      subject{ extended_string.global_match regexp }
+
+      it 'はEnumeratorを返す' do
+        expect( subject ).to be_a Enumerator
+      end
+
+      it 'は文字列のmatchを繰り返しMatchDataを渡す' do
+        expect( regexp ).to receive(:match).with(extended_string, 0).ordered.and_return(match1)
+        expect( regexp ).to receive(:match).with(extended_string, 3).ordered.and_return(match2)
+        expect( regexp ).to receive(:match).with(extended_string, 6).ordered.and_return(match3)
+
+        expect( subject.next ).to eql match1
+        expect( subject.next ).to eql match2
+        expect{ subject.next }.to raise_error StopIteration
+      end
+    end
+
+    context 'ブロックを渡した場合' do
+      subject{ extended_string.global_match(regexp){} }
+
+      it 'はselfを返す' do
+        expect( subject ).to equal extended_string
+      end
+
+      it 'は文字列のmatchを繰り返す' do
+        expect( regexp ).to receive(:match).with(extended_string, 0).ordered.and_return(match1)
+        expect( regexp ).to receive(:match).with(extended_string, 3).ordered.and_return(match2)
+        expect( regexp ).to receive(:match).with(extended_string, 6).ordered.and_return(match3)
+        expect{ |b| extended_string.global_match(regexp, &b) }.to yield_successive_args(match1, match2)
+      end
+    end
+  end
+end
