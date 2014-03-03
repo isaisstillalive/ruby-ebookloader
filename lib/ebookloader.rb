3dadb7b4187b09c440d4c2d2eb4e60a4769fdf7b
isaisstillalive <isaisstillalive@gmail.com>
2014-03-03 16:58:25 +0900

    再び名前変更。Extensions::Stringに変更にするためまずファイルの移動

--- lib/ebookloader.rb
+++ lib/ebookloader.rb
@@ -7,7 +7,7 @@ require 'rubygems'
 require 'faraday'
 
 module EBookloader
-  require_relative 'ebookloader/string_extensions'
+  require_relative 'ebookloader/extensions/string'
   require_relative 'ebookloader/connectable'
   require_relative 'ebookloader/lazy_loadable'
   require_relative 'ebookloader/book_info'
--- spec/extensions/string_spec.rb
+++ spec/extensions/string_spec.rb
@@ -1,6 +1,6 @@
 # coding: utf-8
 
-require_relative 'spec_helper.rb'
+require_relative '../spec_helper.rb'
 
 describe EBookloader::StringExtensions do
   let(:string){ 'abcdef' }
