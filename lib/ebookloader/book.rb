75f936124d83d7f55890a784548f1a1c41680e39
isaisstillalive <isaisstillalive@gmail.com>
2014-03-04 15:28:17 +0900

    Book::Directを削除し、Bookに統合

--- lib/ebookloader/book.rb
+++ lib/ebookloader/book.rb
@@ -4,10 +4,20 @@ module EBookloader
   class Book < Class.new
     Base = self.superclass
 
+    private
+
+    def lazy_load
+      merge title: Pathname(@uri.path).basename.to_s
+    end
+
+    def save_core save_path
+      save_path.parent.mkpath unless save_path.parent.exist?
+      write save_path, uri
+    end
+
     require_relative 'book/base'
     require_relative 'book/multiple_pages'
 
-    require_relative 'book/direct'
     require_relative 'book/acti_book'
     require_relative 'book/akitashoten_reading_communicator'
     require_relative 'book/aoharu'
