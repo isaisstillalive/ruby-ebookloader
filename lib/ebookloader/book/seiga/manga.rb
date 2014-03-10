9470c82851913f3b5a7f59b0750388a367945de1
isaisstillalive <isaisstillalive@gmail.com>
2014-03-11 01:16:46 +0900

    静画マンガの仕様ミスを修正

--- lib/ebookloader/book/seiga/manga.rb
+++ lib/ebookloader/book/seiga/manga.rb
@@ -14,12 +14,12 @@ module EBookloader
 
           author = get_author doc.text('/response/theme/user_id') unless instance_variable_defined? :@author
 
-          update_without_overwrite author: author, title: doc.text('/response/theme/title'), episode: doc.text('/response/theme/episode_title')
+          update_without_overwrite author: author, title: doc.text('/response/theme/content_title'), episode: doc.text('/response/theme/episode_title')
 
           pages_xml = get URI("http://seiga.nicovideo.jp/api/theme/data?theme_id=#{@illust_id}")
           pages_doc = REXML::Document.new pages_xml.body
-          @pages = pages_doc.get_elements('/response/image_list/image').map do |page|
-            Page.new page.text('source_url').gsub(/l\?$/, 'p?'), page: page.text('leaf').to_i
+          @pages = pages_doc.get_elements('/response/image_list/image').map.with_index 1 do |page_data, page|
+            Page.new page_data.text('source_url').gsub(/l\?$/, 'p?'), page: page
           end
 
           true
