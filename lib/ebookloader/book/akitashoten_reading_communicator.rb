873a378d432b06dfba98f4f7146c8bed3ed8aab5
isaisstillalive <isaisstillalive@gmail.com>
2014-03-12 19:00:09 +0900

    変換後にスペースが正しく入らないので微修正

--- lib/ebookloader/book/akitashoten_reading_communicator.rb
+++ lib/ebookloader/book/akitashoten_reading_communicator.rb
@@ -12,8 +12,8 @@ module EBookloader
 
         bookinfo = source.body.match(%r{<h1 title="(?<epsode_num>[^"]*)">[^<]*<span id="subtitle">(?<episode>[^<]*)</span><span id="author" title="(?<author>.*?)">.*<img id="thumbnailImage" src="[^"]*" alt="(?<title>[^"]*)">}m)
         title = bookinfo[:title]
-        episode_num = bookinfo[:epsode_num].gsub(/^#{title}\s/, '')
-        episode = self.class.get_episode_number(episode_num) + bookinfo[:episode]
+        episode_num = bookinfo[:epsode_num].gsub(/^#{title}\s/, '').strip
+        episode = "#{self.class.get_episode_number(episode_num)} #{bookinfo[:episode]}"
         update_without_overwrite title: title, author: bookinfo[:author], episode: episode
 
         match = source.body.match %r{ARC.Comic = (\{.*?url: '(?<image_path>[^']*)'.*?page_count: (?<page_count>\d*).*?\})}m
