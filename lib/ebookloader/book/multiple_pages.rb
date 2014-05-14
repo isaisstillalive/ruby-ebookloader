aa9f86f3255e6a81cab95c6b4a06ee11b67c4ec9
isaisstillalive <isaisstillalive@gmail.com>
2014-05-14 17:57:21 +0900

    zip圧縮時に、フォルダ名のドット以降を拡張子として判別してしまわないように修正

--- lib/ebookloader/book/multiple_pages.rb
+++ lib/ebookloader/book/multiple_pages.rb
@@ -81,7 +81,7 @@ module EBookloader
       # @param dir_path [Pathname] 圧縮元フォルダ
       def zip dir_path
         require 'zip'
-        zip_path = dir_path.sub_ext('.zip')
+        zip_path = dir_path.sub(/$/, '.zip')
         zip_path.delete if zip_path.exist?
 
         Zip::File.open(zip_path, Zip::File::CREATE) do |zipfile|
